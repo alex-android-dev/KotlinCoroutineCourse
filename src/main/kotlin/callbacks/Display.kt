988ed51706e3d8abdd7a8f1e6ec9d53eea5bf37f
avlnkov <alex.lenkov@mail.ru>
2025-04-05 22:20:58 +0300

    добавил String Format

---
+++ src/main/kotlin/callbacks/Display.kt
@@ -0,0 +1,50 @@
+package callbacks
+
+import java.awt.BorderLayout
+import java.awt.Dimension
+import javax.swing.JButton
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.JScrollPane
+import javax.swing.JTextArea
+import kotlin.math.min
+
+object Display {
+
+    private val infoArea = JTextArea().apply {
+        isEditable = false
+    }
+
+    private val loadButton = JButton("Load Book")
+    private val timerLabel = JLabel("Time: 00:00")
+    private val topPanel = JPanel(BorderLayout()).apply {
+        add(timerLabel, BorderLayout.WEST)
+        add(loadButton, BorderLayout.EAST)
+    }
+
+    private val mainFrame = JFrame("Book And Author Info").apply {
+        layout = BorderLayout()
+        add(topPanel, BorderLayout.NORTH)
+        add(JScrollPane(infoArea), BorderLayout.CENTER)
+        size = Dimension(400, 300)
+    }
+
+    fun show() {
+        mainFrame.isVisible = true
+        startTimer()
+    }
+
+    private fun startTimer() {
+        var totalSeconds = 0
+
+        while (true) {
+            val minutes = totalSeconds / 60
+            val seconds = totalSeconds % 60
+            timerLabel.text = String.format("Time: %02d:%02d", minutes, seconds)
+            Thread.sleep(1000)
+            totalSeconds++
+        }
+    }
+
+}
