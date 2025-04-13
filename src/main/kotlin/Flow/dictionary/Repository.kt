33045467506d0e6404ed9e6d0453096050a5fd06
avlnkov <alex.lenkov@mail.ru>
2025-04-13 17:46:41 +0300

    добавил обработку ошибок и закрытие соединения

--- src/main/kotlin/Flow/dictionary/Repository.kt
+++ src/main/kotlin/Flow/dictionary/Repository.kt
@@ -11,26 +11,36 @@ object Repository {
     private const val HEADER_KEY = "X-Api-Key"
 
     fun loadDefinition(word: String): Definition {
-        val urlString = BASE_URL + word // адрес
 
-        val url = URI.create(urlString).toURL() // Преобразуем в объект URL
+        var connection: HttpURLConnection? = null
 
-        val urlConnection = (url.openConnection() as HttpURLConnection).apply {
-            addRequestProperty(
-                HEADER_KEY, API_KEY
-            )
+        return try {
+            val urlString = BASE_URL + word // адрес
+
+            val url = URI.create(urlString).toURL() // Преобразуем в объект URL
+
+            connection = (url.openConnection() as HttpURLConnection).apply {
+                addRequestProperty(
+                    HEADER_KEY, API_KEY
+                )
+            }
+            // Переходим по адресу, открывая соединение
+            // Приводим к HttpURLConnection, чтобы можно было читать данные от сервера
+            // Передаем сюда заголовок с токеном
+
+            val response = connection.inputStream.bufferedReader().readText()
+            // Получаем ответ от сервера.
+            // Поток байтов, который преобразуем в поток символов
+            // JSON
+
+            Json.decodeFromString(response)
+        } catch (e: Exception) {
+            Definition("", "", false)
+        } finally {
+            connection?.disconnect()
         }
-        // Переходим по адресу, открывая соединение
-        // Приводим к HttpURLConnection, чтобы можно было читать данные от сервера
-        // Передаем сюда заголовок с токеном
 
 
-        val response = urlConnection.inputStream.bufferedReader().readText()
-        // Получаем ответ от сервера.
-        // Поток байтов, который преобразуем в поток символов
-        // JSON
-
-        return Json.decodeFromString(response)
     }
 
     // Открыть соединение с интернетом
@@ -43,16 +53,10 @@ fun main() {
     while (true) {
         println("Enter word: ")
 
-        try {
-            val word = readln()
+        val word = readln()
 
-            val definition = Repository.loadDefinition(word)
+        val definition = Repository.loadDefinition(word)
 
-            println(definition.definition)
-        }catch (e: Exception) {
-            println("error")
-        }
-
-
+        println(definition.definition)
     }
 }
