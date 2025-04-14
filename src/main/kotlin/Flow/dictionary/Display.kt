5906b1abcd68379475ffac2febf3364061008686
avlnkov <alex.lenkov@mail.ru>
2025-04-14 22:03:23 +0300

    реализация реактивного стиля с Flow

--- src/main/kotlin/Flow/dictionary/Display.kt
+++ src/main/kotlin/Flow/dictionary/Display.kt
@@ -1,6 +1,7 @@
 package Flow.dictionary
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 import java.awt.BorderLayout
 import java.awt.event.KeyAdapter
 import java.awt.event.KeyEvent
@@ -8,6 +9,9 @@ import java.util.concurrent.Executors
 import javax.swing.*
 
 object Display {
+    private lateinit var queries: Flow<String>
+
+
     private val dispatcher = Executors.newCachedThreadPool().asCoroutineDispatcher()
     private val scope = CoroutineScope(dispatcher + SupervisorJob())
     private val repository = Repository
@@ -53,32 +57,26 @@ object Display {
     }
 
     private fun loadDefinition() {
-        loadingJob?.cancel() // Если пользователь введёт еще один символ, то прошлую корутину нужно отменить
-        loadingJob = scope.launch {
-            resultArea.text = "Loading..."
-            searchButton.isEnabled = false
-            delay(500)
 
-            val word = searchField.text.trim()
-            val text = repository.loadDefinition(word).joinToString("\n\n")
-
-            resultArea.text = text.ifBlank { "Text not found" }
-
-            searchButton.isEnabled = true
-        }
     }
-    /* Есть проблема. Проблема в том, что функция выполняет множество разных операций
-    Это делает невозможным применение данной функции в других частях кода
-    Такая реализация программы называется императивный стиль
 
-    Данную функцию нельзя называть чистой, поскольку она выполняет какие-то действия за её пределами
-    Меняет какое-то внешнее состояние
-
-    Проблема в том, что при работе с чистыми корутинами нам постоянно придется писать в таком стиле
-
-    Flow позволяет писать программы в новом, реактивном стиле
-    */
-
-
+    init {
+        queries
+            .onEach {
+                resultArea.text = "Loading..."
+                searchButton.isEnabled = false
+            } // Вызывается перед стартом потока. В данном случае делаем кнопки неактивными
+            .map {
+                repository.loadDefinition(it)
+            } // Сразу же загружаем слово, которое летит по цепочке. Нам прилетает уже ответ от сервера тут
+            .map {
+                it.joinToString("\n\n").ifEmpty { "Text not found" }
+            }
+            .onEach {
+                resultArea.text = it
+                searchButton.isEnabled = true
+            } // Данные действия нужно выполнять при каждом новом объекте в потоке
+            .launchIn(scope) // У скоупа вызывает метод launch и у Flow вызывает метод collect
+    }
 
 }
